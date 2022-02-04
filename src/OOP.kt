e3f32999cc3aa9ebb354a7b1765ba4596b3da359
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-02-04 18:09:30 +0000

    Starting OOP

---
+++ src/OOP.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var jaf = Person("Mohammad Jafirn", "Hossain")
+}
+
+class Person(firstName: String, lastName: String){
+
+    init {
+        println("ALHAMDULILLAH! First calss created")
+    }
+}
